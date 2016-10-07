7e0367847aa19592bc23339df7b41fe09ba83bbe
Steffen Straub <steffen.straub@kit.edu>
2016-10-07 13:59:57 +0200

    internal bcs specified over different boxes

--- genbox.py
+++ genbox.py
@@ -39,55 +39,129 @@ def write_mesh(file_obj, xstart, xend, ystart, yend, nelx, nely, spatial_dim, dx
         n_prev = n_prev + nelx[i]*nely[i]   # update n_prev
 
 # Set appropriate connecting elements and faces depending on type of boundary conditions
-def set_bc(file_obj, bctype, face, elx, ely, nelx, nely, n_prev):
+def set_bc(file_obj, i, bctype, face, elx, ely, nelx, nely, cor_int_el):
     zero=float(0.0)
 #    pdb.set_trace()
-    cur_el = elx+1 + ely*nely + n_prev
-    # No parameter needed
-    if (bctype == 'W  '): # Wall
+    n_prev = 0 # number of elements in all previous boxes
+    for k in range(0,i):
+        n_prev = n_prev + nelx[i]*nely[i]
+    cur_el = elx+1 + ely*nely[i] + n_prev
+    # No parameters needed
+    if (bctype[i] == 'W  '): # Wall
         file_obj.write(' {0:3s} {1:2d} {2:2d}{3:10.5f}{4:14.5f}{5:14.5f}{6:14.5f}{7:14.5f}{8:s}'\
             .format('W  ',cur_el,face,zero,zero,zero,zero,zero,'\n'))
-    if (bctype == 'SYM'): # Symmetry
+    if (bctype[i] == 'SYM'): # Symmetry
         file_obj.write(' {0:3s} {1:2d} {2:2d}{3:10.5f}{4:14.5f}{5:14.5f}{6:14.5f}{7:14.5f}{8:s}'\
             .format('SYM',cur_el,face,zero,zero,zero,zero,zero,'\n'))
-    if (bctype == 'v  '): # Dirichlet BC for velocity given in userbc
+    if (bctype[i] == 'v  '): # Dirichlet BC for velocity given in userbc
          file_obj.write(' {0:3s} {1:2d} {2:2d}{3:10.5f}{4:14.5f}{5:14.5f}{6:14.5f}{7:14.5f}{8:s}'\
             .format('v  ',cur_el,face,zero,zero,zero,zero,zero,'\n'))
     # Connected element and face is needed
-    if (bctype == 'E  '): # Internal
+    if (bctype[i] == 'E  '): # Internal
         if (face == 1): # south face: connected element is on row lower and conn face is on north
-            conn_el = n_prev + elx + (ely-1)*nely+1
+            conn_el = n_prev + elx + (ely-1)*nely[i]+1
             conn_face = 3
         elif(face == 2):  # east face: connected element is on next column and conn face is west
-            conn_el = n_prev + elx + 1 + ely*nely+1
+            conn_el = n_prev + elx + 1 + ely*nely[i]+1
             conn_face = 4
         elif(face == 3):  # north face: connected element is on row higher and conn face is south
-            conn_el = n_prev + elx + (ely+1)*nely+1
+            conn_el = n_prev + elx + (ely+1)*nely[i]+1
             conn_face = 1
         elif(face == 4):  # west face: connected element is on previous colum and conn face is east
-            conn_el = n_prev + elx-1 + ely*nely+1
+            conn_el = n_prev + elx-1 + ely*nely[i]+1
             conn_face = 2
         file_obj.write(' {0:3s} {1:2d} {2:2d}{3:10.5f}{4:14.5f}{5:14.5f}{6:14.5f}{7:14.5f}{8:s}'\
             .format('E  ',cur_el,face,conn_el,conn_face,zero,zero,zero,'\n'))
-    if (bctype == 'P  '): # Periodic
+    elif (bctype[i] == 'P  '): # Periodic
         conn_el = n_prev + 0
         conn_face = 0
         if (face == 1): # south side: conn el and face are on north side
-            conn_el = n_prev + elx+(nely-1)*nely+1
+            conn_el = n_prev + elx+(nely[i]-1)*nely[i]+1
             conn_face = 3
         elif (face == 2): # east side: conn el and face are on west side
-            conn_el = n_prev + 1+ely*nely
+            conn_el = n_prev + 1+ely*nely[i]
             conn_face = 4
         elif (face == 3): # north side: conn el and face are on south side
-            conn_el = n_prev + elx+0*nely+1
+            conn_el = n_prev + elx+0*nely[i]+1
             conn_face = 1
         elif (face == 4): # west side: conn el and face are on east side
-            conn_el = n_prev + nelx+ely*nely
+            conn_el = n_prev + nelx[i]+ely*nely[i]
             conn_face = 2
         file_obj.write(' {0:3s} {1:2d} {2:2d}{3:10.5f}{4:14.5f}{5:14.5f}{6:14.5f}{7:14.5f}{8:s}'\
             .format('P  ',cur_el,face,conn_el,conn_face,zero,zero,zero,'\n'))
+    # internal BC specified by the user between two boxes
+    elif ('E' in bctype[i] and bctype[i][1:3] != '  '):    
+        # get number of connected boxes
+#        pdb.set_trace()
+        conn_boxes = cor_int_el[bctype[i][1:3]]
+        # choose the other box (different from i)
+        if (conn_boxes[0] == i):
+            conn_box = conn_boxes[1]
+        else:
+            conn_box = conn_boxes[0]
+        # get number of elements in boxes previous to conn_box
+        nel_pre_con = 0
+        for k in range(0,conn_box):
+            nel_pre_con = nel_pre_con + nelx[k]*nely[k]
+##        pdb.set_trace()
+        if (face == 1): 
+            # south face: connected element is on top row in the conn box
+            # and conn face is on north
+            conn_el = nel_pre_con + elx + (nely-1)*nely[conn_box]+1
+            conn_face = 3
+        elif(face == 2):  # east face: connected element is on the first column in the conn box
+            # and conn face is west
+            conn_el = nel_pre_con + 0 + ely*nely[i]+1
+            conn_face = 4
+        elif(face == 3):  # north face: connected element is on the first row in the conn box
+            # and conn face is south
+            conn_el = nel_pre_con + elx + 0*nely[i]+1
+            conn_face = 1
+        elif(face == 4):  # west face: connected element is on  colum and conn face is east
+            conn_el = n_prev + nelx[conn_box] + ely*nely[i]+1
+            conn_face = 2
+        file_obj.write(' {0:3s} {1:2d} {2:2d}{3:10.5f}{4:14.5f}{5:14.5f}{6:14.5f}{7:14.5f}{8:s}'\
+            .format('E  ',cur_el,face,conn_el,conn_face,zero,zero,zero,'\n'))
+
 
 def write_bcs(file_obj, nelx, nely, bcx0, bcx1, bcy0, bcy1, bcz0, bcz1):
+    # Find corresponding user specified internal and periodic BCs between different blocks  
+    cor_intern_el = {}  # dictionary of corresponding internal BCs (each as a pair of the two block numbers)
+    key_intern = 0  # key to this internal BC; they should be numbered: E01, E02, E03
+    for i in range(0,len(nelx)):
+        if ('E' in bcx0[i] and bcx0[i][1:3] != '  '):
+            key_intern = bcx0[i][1:3]
+            # check if key is already there
+            if (key_intern in list(cor_intern_el.keys())):
+                # append second block number to the key
+                cor_intern_el[key_intern] = [cor_intern_el[key_intern], i]
+            else:
+                cor_intern_el[key_intern] = i    # save number of this block
+        if ('E' in bcx1[i] and bcx1[i][1:3] != '  '):
+            key_intern = bcx1[i][1:3]
+            # check if key is already there
+            if (key_intern in list(cor_intern_el.keys())):
+                # append second block number to the key
+                cor_intern_el[key_intern] = [cor_intern_el[key_intern], i]
+            else:
+                cor_intern_el[key_intern] = i    # save number of this block
+        if ('E' in bcy0[i] and bcy0[i][1:3] != '  '):
+            key_intern = bcy0[i][1:3]
+            # check if key is already there
+            if (key_intern in list(cor_intern_el.keys())):
+                # append second block number to the key
+                cor_intern_el[key_intern] = [cor_intern_el[key_intern], i]
+            else:
+                cor_intern_el[key_intern] = i    # save number of this block
+        if ('E' in bcy1[i] and bcy1[i][1:3] != '  '):
+            key_intern = bcy1[i][1:3]
+            # check if key is already there
+            if (key_intern in list(cor_intern_el.keys())):
+                # append second block number to the key
+                cor_intern_el[key_intern] = [cor_intern_el[key_intern], i]
+            else:
+                cor_intern_el[key_intern] = i    # save number of this block
+
     # Loop through all blocks
     n_prev = 0
     for i in range(0,len(nelx)):
@@ -98,16 +172,25 @@ def write_bcs(file_obj, nelx, nely, bcx0, bcx1, bcy0, bcy1, bcz0, bcz1):
                 for face in range(1,5):
                     # check if we are at the boundary
                     if (elx == 0 and face == 4):  # west side
-                        set_bc(file_obj, bcx0[i], face, elx, ely, nelx[i], nely[i], n_prev)
+                        set_bc(file_obj, i, bcx0, face, elx, ely, nelx, nely, \
+                                cor_intern_el)
                     elif (elx == nelx[i]-1 and face == 2):  # east side
-                        set_bc(file_obj, bcx1[i], face, elx, ely, nelx[i], nely[i], n_prev)
+                        set_bc(file_obj, i, bcx1, face, elx, ely, nelx, nely, \
+                                cor_intern_el)
                     elif (ely == 0 and face == 1):  # south side
-                        set_bc(file_obj, bcy0[i], face, elx, ely, nelx[i], nely[i], n_prev)
+                        set_bc(file_obj, i, bcy0, face, elx, ely, nelx, nely, \
+                                cor_intern_el)
                     elif (ely == nely[i]-1 and face == 3):  # north side
-                        set_bc(file_obj, bcy1[i], face, elx, ely, nelx[i], nely[i], n_prev)
+                        set_bc(file_obj, i, bcy1, face, elx, ely, nelx, nely, \
+                                cor_intern_el)
                     # This is the inside
                     else: 
-                        set_bc(file_obj, 'E  ', face, elx, ely, nelx[i], nely[i], n_prev)
+                        # populate list for internal bctype
+                        bc_int = []
+                        for k in range(0,len(nelx)):
+                            bc_int.append('E  ')
+                        set_bc(file_obj, i, bc_int, face, elx, ely, nelx, nely, \
+                                cor_intern_el)
         n_prev = n_prev + nelx[i]*nely[i]
 
 # Read the .box file
